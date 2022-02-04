39a3a88a579884c8ace7b2618ab389edd88bfe04
Alan Duncan <duncan.alan@me.com>
2022-02-04 16:54:55 -0500

    Improve script to deploy

--- scripts/prep_ojisan_css.py
+++ scripts/prep_ojisan_css.py
@@ -1,4 +1,21 @@
-#!/Users/alan/.pyenv/shims/python3
+#!/usr/bin/env python3
+
+"""
+Usage:
+   prep_ojisan_css.py -s <source> -o <dest> -m <manifest>
+
+Options:
+    -s <source> --source <source>       Source path for template css files
+    -o <dest> --out <dest>              Path for versioned css files
+    -m <manifest> --manifest <manifest> Path for manifest css.json file
+    - h, --help                         Show this help
+
+
+"""
+
+# Typical call:
+#./prep_ojisan_css.py "/Users/alan/Documents/blog/ojisan/css_source" \
+#    "/Users/alan/Documents/blog/ojisan/static/css" "/Users/alan/Documents/blog/ojisan/data/tonedeaf"
 
 import os
 import fnmatch
@@ -7,6 +24,7 @@ import json
 import sys
 import random
 import re
+from docopt import docopt
 from shutil import copyfile
 
 def fileHash(filePath):
@@ -24,11 +42,35 @@ def replaceIdentifier(fn,path):
     identifier = fileHash(os.path.join(path, fn)).strip('\n')
     return baseName + "_" + identifier + ".css"
 
+
+arguments = docopt(__doc__, version='ruwordle 0.2')
+try:
+  path = os.path.abspath(arguments['--source'])
+except KeyError:
+  print('Error: no source path provided')
+  sys.exit(0)
+
+try:
+  out_path = os.path.abspath(arguments['--out'])
+except KeyError:
+  print('Error: no output path provided')
+  sys.exit(0)
+
+try:
+  j_data = os.path.abspath(arguments['--manifest'])
+  j_data = os.path.join(j_data,"css.json")
+except KeyError:
+  print('Error: no manifest path provided')
+  sys.exit(0)
+
 data = {}
-path = os.path.abspath(sys.argv[1])
-out_path = os.path.abspath(sys.argv[2])
-j_data = os.path.abspath(sys.argv[3])
-j_data = os.path.join(j_data,"css.json")
+# the 1st argument `path` is the source path css_path
+# path = os.path.abspath(sys.argv[1])
+# # the 2nd argument `out_path` is the path to the actual css dir
+# out_path = os.path.abspath(sys.argv[2])
+# # the 3rd argument is the css.json manifest path
+# j_data = os.path.abspath(sys.argv[3])
+# j_data = os.path.join(j_data,"css.json")
 
 
 # empty the primary css directory
